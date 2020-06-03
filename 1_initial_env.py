13380c0dda4762a3edede77dc394feb7277f68b6
LT <liutao272@gmail.com>
2020-06-03 20:36:46 +0800

    reinforcement learning in python3

---
+++ 1_initial_env.py
@@ -0,0 +1,145 @@
+import gym
+
+# initial enviroment
+env = gym.make("Taxi-v3").env
+env.render()
+
+env.reset() # reset environment to a new, random state
+env.render()
+
+print("Action Space {}".format(env.action_space))
+print("State Space {}".format(env.observation_space))
+
+state = env.encode(3, 1, 2, 0) # (taxi row, taxi column, passenger index, destination index)
+print("State:", state)
+
+env.s = state
+env.render()
+
+env.P[328]
+
+env.s = 328  # set environment to illustration's state
+
+epochs = 0
+penalties, reward = 0, 0
+
+frames = [] # for animation
+
+done = False
+
+while not done:
+    action = env.action_space.sample()
+    state, reward, done, info = env.step(action)
+
+    if reward == -10:
+        penalties += 1
+    
+    # Put each rendered frame into dict for animation
+    frames.append({
+        'frame': env.render(mode='ansi'),
+        'state': state,
+        'action': action,
+        'reward': reward
+        }
+    )
+
+    epochs += 1
+    
+    
+print("Timesteps taken: {}".format(epochs))
+print("Penalties incurred: {}".format(penalties))
+print(type(frames[0]['frame']))
+
+from IPython.display import clear_output
+from time import sleep
+
+def print_frames(frames):
+    for i, frame in enumerate(frames):
+        clear_output(wait=True)
+        print(frame['frame'])
+        print("Timestep: {}".format(i + 1))
+        print("State: {}".format(frame['state']))
+        print("Action: {}".format(frame['action']))
+        print("Reward: {}".format(frame['reward']))
+        sleep(.1)
+        
+print_frames(frames)
+
+import numpy as np
+q_table = np.zeros([env.observation_space.n, env.action_space.n])
+import timeit
+timeit.timeit()
+"""Training the agent"""
+
+import random
+from IPython.display import clear_output
+
+# Hyperparameters
+alpha = 0.1
+gamma = 0.6
+epsilon = 0.1
+
+# For plotting metrics
+all_epochs = []
+all_penalties = []
+
+for i in range(1, 100001):
+    state = env.reset()
+
+    epochs, penalties, reward, = 0, 0, 0
+    done = False
+    
+    while not done:
+        if random.uniform(0, 1) < epsilon:
+            action = env.action_space.sample() # Explore action space
+        else:
+            action = np.argmax(q_table[state]) # Exploit learned values
+
+        next_state, reward, done, info = env.step(action) 
+        
+        old_value = q_table[state, action]
+        next_max = np.max(q_table[next_state])
+        
+        new_value = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)
+        q_table[state, action] = new_value
+
+        if reward == -10:
+            penalties += 1
+
+        state = next_state
+        epochs += 1
+        
+    if i % 100 == 0:
+        clear_output(wait=True)
+        print("Episode: {}".format(i))
+
+print("Training finished.\n")
+
+q_table[328]
+
+"""Evaluate agent's performance after Q-learning"""
+
+total_epochs, total_penalties = 0, 0
+episodes = 100
+
+for _ in range(episodes):
+    state = env.reset()
+    epochs, penalties, reward = 0, 0, 0
+    
+    done = False
+    
+    while not done:
+        action = np.argmax(q_table[state])
+        state, reward, done, info = env.step(action)
+
+        if reward == -10:
+            penalties += 1
+
+        epochs += 1
+
+    total_penalties += penalties
+    total_epochs += epochs
+
+print("Results after {} episodes:".format(episodes))
+print("Average timesteps per episode: {}".format(total_epochs / episodes))
+print("Average penalties per episode: {}".format(total_penalties / episodes))
---
+++ dependencies.sh
@@ -0,0 +1,1 @@
+pip3 install cmake 'gym[atari]' scipy
